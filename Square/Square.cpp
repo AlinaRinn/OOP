2524737226c01f03a118b42bac3a53b93533b8ca
AlinaRinn <nik17042001@rambler.ru>
2022-12-02 01:25:07 +0300

    Micro offtop program

---
+++ Square/Square.cpp
@@ -0,0 +1,27 @@
+﻿// Алгоритм быстрого возведения в степень
+
+#include <iostream>
+
+float power(float value, int pow) {
+	float result = 1;
+	while (pow > 0) {
+		if (pow % 2 == 1) {
+			result *= value;
+		}
+		value *= value;
+		pow /= 2;
+	}
+	return result;
+}
+
+int main()
+{
+	float value = 0;
+	int	pow = 0;
+
+	std::cin >> value;
+	std::cin >> pow;
+	std::cout << power(value, pow);
+}
+
+
